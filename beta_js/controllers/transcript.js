ae1d087322c7e35374cfc255e3df7af454866660
Yan Ren <ryan.ren3@gmail.com>
2017-10-31 20:17:32 -0400

    commit models

---
+++ beta_js/controllers/transcript.js
@@ -0,0 +1,5 @@
+/* global app, database */
+
+app.get( '/transcript', function( request, response ) {
+  response.send("This is where you see the transcript!");
+})
---
+++ beta_js/models/srt.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+
+//uploaded files schema
+var srtSchedma = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    requried: true
+  },
+  upload_date: {
+    type: Date,
+    default: Date.now
+  },
+  // file link: link to a uploaded file
+  file_link:{
+    "type": mongoose.Schema.Types.ObjectId, "ref": "uploads"
+  }
+});
+
+const Srt = module.exports = mongoose.model('srt', srtSchedma);
+
+// Get Srts
+module.exports.getSrt = (callback, limit) => {
+	Srt.find(callback).limit(limit);
+}
+// Get Srt
+module.exports.getSrtById = (id, callback) => {
+  Srt.findById(id, callback);
+}
+
+// Add Srt
+module.exports.addSrt = (upload, callback) => {
+	Srt.create(upload, callback);
+}
+
+// // Update Genre
+// module.exports.updateGenre = (id, genre, options, callback) => {
+// 	var query = {_id: id};
+// 	var update = {
+// 		name: genre.name
+// 	}
+// 	Genre.findOneAndUpdate(query, update, options, callback);
+// }
+
+
+// // Delete Genre
+// module.exports.removeGenre = (id, callback) => {
+// 	var query = {_id: id};
+// 	Genre.remove(query, callback);
+// }
