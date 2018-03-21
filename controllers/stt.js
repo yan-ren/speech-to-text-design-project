7b02cf48386d023164e4f54021c3150ef1e8b929
Yan Ren <ryan.ren3@gmail.com>
2018-03-21 19:04:38 -0400

    bug fixed: not pull all reesults

--- controllers/stt.js
+++ controllers/stt.js
@@ -49,55 +49,56 @@ app.post('/stt/gcs', function( req, res ) {
       config: config,
       audio: audio,
     }
-    client.longRunningRecognize(request)
-      .then(responses => {
-        var operation = responses[0];
-        var initialApiResponse = responses[1];
-
-        // Adding a listener for the "complete" event starts polling for the
-        // completion of the operation.
-        operation.on('complete', (result, metadata, finalApiResponse) => {
-          // console.log('result: ');
-          // console.log(JSON.stringify(result));
-          // console.log('metadata: ');
-          // console.log(JSON.stringify(metadata));
-          // console.log('finalApiResponse: ');
-          // console.log(JSON.stringify(finalApiResponse));
-          // save to db srt collection
-           db_Sub.addSub({
-               media_id: req.body.id,
-//               content: JSON.stringify(res, null, 2)
-               content: result.results[0].alternatives[0].transcript
-             }, function(err, doc){
-               if(err){  throw err;  }
-
-               // update db file collection
-               // new: bool - if true, return the modified document rather than the original. defaults to false
-               db_Media.updateMedia(req.body.id, {transcribe: true,
-                                         sub: doc._id}, {new: false},
-                                         function(err){
-                                           if(err){  console.log(err); throw err;  }
-                                         }
-                                   );
-             }); // addSub
-           // response.send(JSON.stringify(res, null, 2));
-           res.end('success');
-
+    client
+      .longRunningRecognize(request)
+      .then(data => {
+        const operation = data[0];
+        // Get a Promise representation of the final result of the job
+        return operation.promise();
+      })
+      .then(data => {
+        const response = data[0];
+        var subContent = "";
+        response.results.forEach(result => {
+          console.log(`Transcription: ${result.alternatives[0].transcript}`);
+          subContent = subContent + result.alternatives[0].transcript;
+          result.alternatives[0].words.forEach(wordInfo => {
+            // NOTE: If you have a time offset exceeding 2^32 seconds, use the
+            // wordInfo.{x}Time.seconds.high to calculate seconds.
+            const startSecs =
+              `${wordInfo.startTime.seconds}` +
+              `.` +
+              wordInfo.startTime.nanos / 100000000;
+            const endSecs =
+              `${wordInfo.endTime.seconds}` +
+              `.` +
+              wordInfo.endTime.nanos / 100000000;
+            console.log(`Word: ${wordInfo.word}`);
+            console.log(`\t ${startSecs} secs - ${endSecs} secs`);
+          });
         });
 
-        // Adding a listener for the "progress" event causes the callback to be
-        // called on any change in metadata when the operation is polled.
-        operation.on('progress', (metadata, apiResponse) => {
-          // doSomethingWith(metadata)
-        });
+        db_Sub.addSub({
+            media_id: req.body.id,
+    //               content: JSON.stringify(res, null, 2)
+            content: subContent
+          }, function(err, doc){
+            if(err){  throw err;  }
 
-        // Adding a listener for the "error" event handles any errors found during polling.
-        operation.on('error', err => {
-          throw(err);
-        });
+            // update db file collection
+            // new: bool - if true, return the modified document rather than the original. defaults to false
+            db_Media.updateMedia(req.body.id, {transcribe: true,
+                                      sub: doc._id}, {new: false},
+                                      function(err){
+                                        if(err){  console.log(err); throw err;  }
+                                      }
+                                );
+          }); // addSub
+        // response.send(JSON.stringify(res, null, 2));
+        res.end('success');
       })
       .catch(err => {
-        console.error(err);
+        console.error('ERROR:', err);
       });
   });
 
--- test/test_stt.js
+++ test/test_stt.js
@@ -15,11 +15,11 @@ const client = speech.v1({
   // const encoding = 'Eencoding of the audio file, e.g. LINEAR16';
   // const sampleRateHertz = 16000;
   // const languageCode = 'BCP-47 language code, e.g. en-US';
-  const gcsUri = 'gs://speech-to-text-sandbox/news_44s.flac';
+  const gcsUri = 'gs://speech-to-text-sandbox/fr_2m6s.flac';
   const encoding = 'FLAC';
 //  const sampleRateHertz = 48000;
   //  	fr-CA en-US
-  const languageCode = 'en-US';
+  const languageCode = 'fr-CA';
 
   const config = {
     enableWordTimeOffsets: true,
@@ -36,58 +36,111 @@ const client = speech.v1({
     audio: audio,
 };
 
-client.longRunningRecognize(request)
-  .then(responses => {
-    var operation = responses[0];
-    var initialApiResponse = responses[1];
+client
+  .longRunningRecognize(request)
+  .then(data => {
+    const operation = data[0];
+    // Get a Promise representation of the final result of the job
+    return operation.promise();
+  })
+  .then(data => {
+    const response = data[0];
+    var subContent = "";
+    response.results.forEach(result => {
+      console.log(`Transcription: ${result.alternatives[0].transcript}`);
+      subContent = subContent + result.alternatives[0].transcript;
+      result.alternatives[0].words.forEach(wordInfo => {
+        // NOTE: If you have a time offset exceeding 2^32 seconds, use the
+        // wordInfo.{x}Time.seconds.high to calculate seconds.
+        const startSecs =
+          `${wordInfo.startTime.seconds}` +
+          `.` +
+          wordInfo.startTime.nanos / 100000000;
+        const endSecs =
+          `${wordInfo.endTime.seconds}` +
+          `.` +
+          wordInfo.endTime.nanos / 100000000;
+        console.log(`Word: ${wordInfo.word}`);
+        console.log(`\t ${startSecs} secs - ${endSecs} secs`);
 
-    // Adding a listener for the "complete" event starts polling for the
-    // completion of the operation.
-    operation.on('complete', (result, metadata, finalApiResponse) => {
-      console.log('result');
-      console.log(JSON.stringify(result));
-      console.log('metadata');
-      console.log(JSON.stringify(metadata));
-      console.log('finalApiResponse');
-      console.log(JSON.stringify(finalApiResponse));
+      });
     });
 
-    // Adding a listener for the "progress" event causes the callback to be
-    // called on any change in metadata when the operation is polled.
-    operation.on('progress', (metadata, apiResponse) => {
-      // doSomethingWith(metadata)
-    });
+    db_Sub.addSub({
+        media_id: req.body.id,
+//               content: JSON.stringify(res, null, 2)
+        content: subContent
+      }, function(err, doc){
+        if(err){  throw err;  }
 
-    // Adding a listener for the "error" event handles any errors found during polling.
-    operation.on('error', err => {
-      // throw(err);
-    });
+        // update db file collection
+        // new: bool - if true, return the modified document rather than the original. defaults to false
+        db_Media.updateMedia(req.body.id, {transcribe: true,
+                                  sub: doc._id}, {new: false},
+                                  function(err){
+                                    if(err){  console.log(err); throw err;  }
+                                  }
+                            );
+      }); // addSub
+    // response.send(JSON.stringify(res, null, 2));
+    res.end('success');
   })
   .catch(err => {
-    console.error(err);
+    console.error('ERROR:', err);
   });
 
-// method 2
-  client.longRunningRecognize(request)
-     .then(data => {
-       const operation = data[0];
-       // Get a Promise representation of the final result of the job
-       return operation.promise();
-     })
-     .then(data => {
-       const response = data[0];
-       const transcription = response.results
-         .map(result => result.alternatives[0].transcript)
-         .join('\n');
+// client.longRunningRecognize(request)
+//   .then(responses => {
+//     var operation = responses[0];
+//     var initialApiResponse = responses[1];
+//
+//     // Adding a listener for the "complete" event starts polling for the
+//     // completion of the operation.
+//     operation.on('complete', (result, metadata, finalApiResponse) => {
+//       console.log('result');
+//       console.log(JSON.stringify(result));
+//       // console.log('metadata');
+//       // console.log(JSON.stringify(metadata));
+//       // console.log('finalApiResponse');
+//       // console.log(JSON.stringify(finalApiResponse));
+//     });
+//
+//     // Adding a listener for the "progress" event causes the callback to be
+//     // called on any change in metadata when the operation is polled.
+//     operation.on('progress', (metadata, apiResponse) => {
+//       // doSomethingWith(metadata)
+//     });
+//
+//     // Adding a listener for the "error" event handles any errors found during polling.
+//     operation.on('error', err => {
+//       // throw(err);
+//     });
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
 
-       //TODO: save to db
-       console.log('raw data: ' + data);
-       console.log(`Transcription: ${transcription}`);
-/*
-raw data: .google.cloud.speech.v1.LongRunningRecognizeResponse,.google.cloud.speech.v1.LongRunningRecognizeMetadata,[object Object]
-Transcription: the more than half a Century men and boys I've been picking up at the Australian political scene like an office on the garbage bin and now it is for the hot both heavy and light that I approach the microphone for what is the last time looking back at the political week and the last time on it I am at the end of 42 years is a journalist which began on a summer Monday morning at the end of 1975 when I walk into Rupert Murdoch's news limited building took the list to the 4th floor turn left and left into the partition Newsroom of the Daily Telegraph and joined in what was then in part the demolition of the few remaining shattered Parts the half pillars of the whitlam government
-*/
-     })
-     .catch(err => {
-       console.error('ERROR:', err);
-     });
+// method 2
+//   client.longRunningRecognize(request)
+//      .then(data => {
+//        const operation = data[0];
+//        // Get a Promise representation of the final result of the job
+//        return operation.promise();
+//      })
+//      .then(data => {
+//        const response = data[0];
+//        const transcription = response.results
+//          .map(result => result.alternatives[0].transcript)
+//          .join('\n');
+//
+//        //TODO: save to db
+//        console.log('raw data: ' + data);
+//        console.log(`Transcription: ${transcription}`);
+// /*
+// raw data: .google.cloud.speech.v1.LongRunningRecognizeResponse,.google.cloud.speech.v1.LongRunningRecognizeMetadata,[object Object]
+// Transcription: the more than half a Century men and boys I've been picking up at the Australian political scene like an office on the garbage bin and now it is for the hot both heavy and light that I approach the microphone for what is the last time looking back at the political week and the last time on it I am at the end of 42 years is a journalist which began on a summer Monday morning at the end of 1975 when I walk into Rupert Murdoch's news limited building took the list to the 4th floor turn left and left into the partition Newsroom of the Daily Telegraph and joined in what was then in part the demolition of the few remaining shattered Parts the half pillars of the whitlam government
+// */
+//      })
+//      .catch(err => {
+//        console.error('ERROR:', err);
+//      });
